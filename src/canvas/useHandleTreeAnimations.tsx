156c665fb371d5cfd61f92f668ae1a0747a976eb
Lucas Pennice <lucaspennice@gmail.com>
2023-03-13 15:38:44 -0300

    end animation

--- src/canvas/useHandleTreeAnimations.tsx
+++ src/canvas/useHandleTreeAnimations.tsx
@@ -1,4 +1,4 @@
-import { useDerivedValue, useSharedValue, withSpring, withTiming } from "react-native-reanimated";
+import { Easing, useDerivedValue, useSharedValue, withDelay, withSpring, withTiming } from "react-native-reanimated";
 import { mix, useSharedValueEffect, useValue, clamp, useComputedValue } from "@shopify/react-native-skia";
 import { CIRCLE_SIZE } from "./parameters";
 import { useEffect } from "react";
@@ -6,51 +6,60 @@ import { CIRCLE_SIZE_SELECTED } from "./Tree";
 import { Book, TreeNode } from "../types";
 
 const useHandleTreeAnimations = (selectedNode: string | null, tree: TreeNode<Book>, currentNodeCoordintes: { x: number; y: number }) => {
-    const innerCircleRadius = useValue(CIRCLE_SIZE);
-    const outerCircleRadius = useValue(CIRCLE_SIZE * 1.2);
     const circleBlur = useValue(0);
     const pathBlur = useValue(0);
+    const shouldTransform = useValue(0);
     const pathTrim = useValue(0);
     //
     const isActive = useSharedValue(0);
+    const isBorderTraced = useSharedValue(0);
     const isBlurred = useSharedValue(0);
     const isPathBlurred = useSharedValue(0);
 
     useEffect(() => {
         const shouldActivate = selectedNode === tree.node.id;
-        const shouldBlur = selectedNode !== tree.node.id && selectedNode !== null;
-        const shouldBlurPath = selectedNode !== null;
 
         isActive.value = withSpring(shouldActivate ? 1 : 0, { damping: 18, stiffness: 300 });
-        isBlurred.value = withTiming(shouldBlur ? 1 : 0, { duration: 0.15 });
-        isPathBlurred.value = withTiming(shouldBlurPath ? 1 : 0, { duration: 150 });
-    }, [isActive, isPathBlurred, isBlurred, selectedNode]);
 
-    const scale = useComputedValue(
-        //Im using path r
-        () => [
-            {
-                scale: mix(pathTrim.current, 1, 3),
-            },
-        ],
-        [pathTrim]
-    );
+        isBorderTraced.value = withTiming(shouldActivate ? 1 : 0, { duration: 300, easing: Easing.sin });
+    }, [selectedNode]);
 
     useSharedValueEffect(() => {
-        innerCircleRadius.current = mix(isActive.value, CIRCLE_SIZE, CIRCLE_SIZE_SELECTED);
-        outerCircleRadius.current = mix(isActive.value, CIRCLE_SIZE * 1.2, CIRCLE_SIZE_SELECTED * 1.2);
-        pathTrim.current = clamp(mix(isActive.value, 0, 1), 0, 1);
+        shouldTransform.current = mix(isActive.value, 0, 1);
     }, isActive);
+
+    // Handles circle blur 👇
+
+    useEffect(() => {
+        const shouldBlur = selectedNode !== tree.node.id && selectedNode !== null;
+
+        isBlurred.value = withTiming(shouldBlur ? 1 : 0, { duration: 0.15 });
+    }, [selectedNode]);
 
     useSharedValueEffect(() => {
         circleBlur.current = mix(isBlurred.value, 0, 10);
     }, isBlurred);
 
+    //Handles path blur 👇
+    useEffect(() => {
+        const shouldBlurPath = selectedNode !== null;
+
+        isPathBlurred.value = withTiming(shouldBlurPath ? 1 : 0, { duration: 150 });
+    }, [selectedNode]);
+
     useSharedValueEffect(() => {
         pathBlur.current = mix(isPathBlurred.value, 0, 10);
     }, isPathBlurred);
 
-    return { innerCircleRadius, outerCircleRadius, circleBlur, pathBlur, pathTrim, scale };
+    //Handles path trim 👇
+    useSharedValueEffect(() => {
+        pathTrim.current = isBorderTraced.value;
+    }, isBorderTraced);
+
+    const groupTransform = useComputedValue(() => [{ scale: mix(shouldTransform.current, 1, 3) }], [shouldTransform]);
+    //
+
+    return { circleBlur, pathBlur, pathTrim, groupTransform };
 };
 
 export default useHandleTreeAnimations;
