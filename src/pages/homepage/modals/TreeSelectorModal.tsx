d25c4726b76886ef13bc4587ef89b763cd354836
Lucas Pennice <lucaspennice@gmail.com>
2023-04-03 22:05:59 -0300

    change tree selector design

--- src/pages/homepage/modals/TreeSelectorModal.tsx
+++ src/pages/homepage/modals/TreeSelectorModal.tsx
@@ -1,8 +1,11 @@
-import { Button, Modal, View } from "react-native";
-import { hideLabel, selectCanvasDisplaySettings, toggleTreeSelector } from "../../../redux/canvasDisplaySettingsSlice";
+import { BlurView } from "expo-blur";
+import { Modal, SafeAreaView, TouchableHighlight, View } from "react-native";
+import AppText from "../../../AppText";
+import { selectCanvasDisplaySettings, toggleTreeSelector } from "../../../redux/canvasDisplaySettingsSlice";
 import { changeTree, unselectTree } from "../../../redux/currentTreeSlice";
 import { useAppDispatch, useAppSelector } from "../../../redux/reduxHooks";
 import { centerFlex, mockSkillTreeArray } from "../../../types";
+import { colors } from "../canvas/parameters";
 
 function TreeSelectorModal() {
     const { openMenu } = useAppSelector(selectCanvasDisplaySettings);
@@ -11,26 +14,38 @@ function TreeSelectorModal() {
 
     return (
         <Modal animationType="slide" transparent={true} visible={openMenu == "treeSelector"} onRequestClose={() => dispatch(toggleTreeSelector())}>
-            <View style={[centerFlex, { flex: 1, backgroundColor: "white" }]}>
-                <Button onPress={() => dispatch(toggleTreeSelector())} title="close!" />
-                <Button onPress={() => dispatch(unselectTree())} title="Unselect" />
-                {mockSkillTreeArray.map((tree, idx) => {
-                    return (
-                        <Button
-                            key={idx}
-                            onPress={() => {
-                                dispatch(unselectTree());
-                                dispatch(toggleTreeSelector());
-                                dispatch(hideLabel());
-                                setTimeout(() => {
-                                    dispatch(changeTree(tree));
-                                }, 100);
-                            }}
-                            title={`Select ${tree.treeName ?? "Tree"}`}
-                        />
-                    );
-                })}
-            </View>
+            <SafeAreaView style={[{ flex: 1 }]}>
+                <BlurView intensity={30} style={[{ flex: 1, padding: 20 }]}>
+                    <View style={[centerFlex, { flexDirection: "row", justifyContent: "space-between", marginBottom: 30 }]}>
+                        <TouchableHighlight
+                            onPress={() => dispatch(unselectTree())}
+                            style={[centerFlex, { paddingVertical: 15, borderRadius: 10, backgroundColor: colors.background, width: 150 }]}>
+                            <AppText style={{ fontSize: 20, color: `${colors.blue}` }}>Unselect All</AppText>
+                        </TouchableHighlight>
+                        <TouchableHighlight
+                            onPress={() => dispatch(toggleTreeSelector())}
+                            style={[centerFlex, { paddingVertical: 15, borderRadius: 10, backgroundColor: colors.background, width: 150 }]}>
+                            <AppText style={{ fontSize: 20, color: `${colors.accent}` }}>Close</AppText>
+                        </TouchableHighlight>
+                    </View>
+                    {mockSkillTreeArray.map((tree, idx) => {
+                        return (
+                            <TouchableHighlight
+                                key={idx}
+                                onPress={() => {
+                                    dispatch(unselectTree());
+                                    dispatch(toggleTreeSelector());
+                                    setTimeout(() => {
+                                        dispatch(changeTree(tree));
+                                    }, 100);
+                                }}
+                                style={{ width: "100%", padding: 25, borderRadius: 10, backgroundColor: colors.background, marginBottom: 15 }}>
+                                <AppText style={{ fontSize: 20, color: `white` }}>{tree.treeName ?? "Tree"}</AppText>
+                            </TouchableHighlight>
+                        );
+                    })}
+                </BlurView>
+            </SafeAreaView>
         </Modal>
     );
 }
