265b11fea4a0d8ca73b78e63542878762a5d866a
Lucas Pennice <lucaspennice@gmail.com>
2023-10-03 15:48:07 -0300

    ya no crashea

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,8 +1,8 @@
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { useFonts } from "expo-font";
-import { SplashScreen, Stack } from "expo-router";
+import { SplashScreen } from "expo-router";
 import { useEffect } from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 
 export default function RootLayout() {
     const [loaded, error] = useFonts({
@@ -23,13 +23,20 @@ export default function RootLayout() {
     }, [loaded]);
 
     if (!loaded) return <Text>Loading...</Text>;
+    // ME PARECE QUE EL PROBLEMA ESTA EN TENER UN TABS ADENTRO DE UN STACK ?
+    //     O AL MENOS AL PROGRAMA NO LE FUSTA TENER UN STACK ADENTRO DE UN STACK ADENTRO DE UM<TABS>
+    //     HABRIA QUE IR PROBANDO QUE ES
 
-    return (
-        <Stack
-            screenOptions={{
-                headerShown: false,
-            }}>
-            <Stack.Screen name="(tabs)" />
-        </Stack>
-    );
+    return <View style={{ backgroundColor: "green", width: 100, height: 100 }}></View>;
+
+    // return (
+    //     <View style={{ backgroundColor: "green", width: 400, height: 400 }}></View>
+    // return (
+    // <Stack
+    //     screenOptions={{
+    //         headerShown: false,
+    //     }}>
+    //     <Stack.Screen name="(tabs)" />
+    // </Stack>
+    // );
 }
