2b751054bd2c6eba29c8611faf4456b641a2e348
Lucas Pennice <lucaspennice@gmail.com>
2023-11-16 10:35:19 -0300

    create useMongoCompliantUserId hook

---
+++ src/useMongoCompliantUserId.tsx
@@ -0,0 +1,31 @@
+import { useAuth } from "@clerk/clerk-expo";
+
+const idToHexString = (userId: string) => {
+    const encoder = new TextEncoder();
+
+    const uint8Array = encoder.encode(userId);
+
+    const hexEncoded: string = Array.from(uint8Array)
+        .map((byte) => byte.toString(16).padStart(2, "0"))
+        .join("");
+
+    return hexEncoded;
+};
+
+// const hexToOriginalString = (hexString: string) => {
+//     const hexDecodedUint8Array = new Uint8Array(hexString.match(/.{1,2}/g)!.map((byte) => parseInt(byte, 16)));
+//     const decoder = new TextDecoder();
+//     const decodedString: string = decoder.decode(hexDecodedUint8Array);
+
+//     return decodedString;
+// };
+
+function useMongoCompliantUserId() {
+    const { userId } = useAuth();
+
+    if (userId === null || userId === undefined) return "";
+
+    return idToHexString(userId);
+}
+
+export default useMongoCompliantUserId;
