ef62b7fcade36e1d1b121625b69df695245c8b95
Lucas Pennice <lucaspennice@gmail.com>
2023-12-05 19:55:51 -0300

    install react native purchase app version increase

--- app.config.js
+++ app.config.js
@@ -6,7 +6,7 @@ module.exports = {
         typedRoutes: true,
         tsconfigPaths: true,
     },
-    version: "1.0.57",
+    version: "1.0.58",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
@@ -41,7 +41,7 @@ module.exports = {
             "android.permission.ACCESS_MEDIA_LOCATION",
             "com.google.android.gms.permission.AD_ID",
         ],
-        versionCode: 63,
+        versionCode: 64,
         package: "com.lucaspennice.skilltrees",
         intentFilters: [
             {
