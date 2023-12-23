52703307babf14f7e30042b692203c772929fe48
Lucas Pennice <lucaspennice@gmail.com>
2023-12-23 10:58:12 -0300

    version increase

--- app.config.js
+++ app.config.js
@@ -6,7 +6,7 @@ module.exports = {
         typedRoutes: true,
         tsconfigPaths: true,
     },
-    version: "1.1.0",
+    version: "1.1.1",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
@@ -39,7 +39,7 @@ module.exports = {
             "android.permission.ACCESS_MEDIA_LOCATION",
             "com.google.android.gms.permission.AD_ID",
         ],
-        versionCode: 65,
+        versionCode: 66,
         package: "com.lucaspennice.skilltrees",
         intentFilters: [
             {
